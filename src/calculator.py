5cbdf28155bf8bb6b4947cae9e9b26b9f1ec7753
RaoufAddeche <addeche.raouf@gmail.com>
2025-05-13 16:24:59 +0200

    start project fort ci-cd

---
+++ src/calculator.py
@@ -0,0 +1,45 @@
+def add(a : float, b: float) -> float:
+    return a + b
+
+def substract(a: float, b: float) -> float:
+    return a - b
+
+
+def multiply(a: float, b: float) -> float:
+    return a * b
+
+def divide(a: float, b: float) -> float:
+    if b == 0:
+        raise ValueError ("Impossible de diviser par 0")
+    return a / b
+
+choix = 100
+while True : 
+    print("#"*10, "Calculatrice", "#"*10)
+    print()
+    print("1 - Addition \n2 - Soustraction \n3 - Division \n4 - Multiplication")
+    choix = int(input("Saisissez un nombre (0 pour quitter) : "))
+    if choix == 0 :
+        print("Meric d'avoir utilisé notre calculatrice. Au revoir")
+        print("#"*40)
+        break
+    print()
+    a = float(input("premier nombre : "))
+    b = float(input("second nombre : "))
+    
+    if choix == 1 : 
+        print("résultat :", add(a,b))
+    
+    elif choix == 2 : 
+        print("résultat :", substract(a,b))
+        
+    elif choix == 3 : 
+        print("résultat :", multiply(a,b))
+        
+    elif choix == 4 : 
+        print("résultat :", divide(a,b))
+        
+    print("-"*30)
+
+if __name__ == "__main__":
+    print("Calculator module")
