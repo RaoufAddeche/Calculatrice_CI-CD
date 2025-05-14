d38e786c1bd2ddcea9251877c0c6fe25841ce7a8
RaoufAddeche <addeche.raouf@gmail.com>
2025-05-14 10:09:17 +0200

    test

--- src/calculator.py
+++ src/calculator.py
@@ -14,37 +14,3 @@ def divide(a: float, b: float) -> float:
     if b == 0:
         raise ValueError("Impossible de diviser par 0")
     return a / b
-
-
-choix = 100
-while True:
-    print("#" * 10, "Calculatrice", "#" * 10)
-    print()
-    print(
-        """1 - Addition " \
-    "\n2 - Soustraction \n" \
-    "3 - Division \n" \
-    "4 - Multiplication"""
-    )
-    choix = int(input("Saisissez un nombre (0 pour quitter) : "))
-    if choix == 0:
-        print("Meric d'avoir utilisé notre calculatrice. Au revoir")
-        print("#" * 40)
-        break
-    print()
-    a = float(input("premier nombre : "))
-    b = float(input("second nombre : "))
-
-    if choix == 1:
-        print("résultat :", add(a, b))
-
-    elif choix == 2:
-        print("résultat :", subtract(a, b))
-
-    elif choix == 3:
-        print("résultat :", multiply(a, b))
-
-    elif choix == 4:
-        print("résultat :", divide(a, b))
-
-    print("-" * 30)
---
+++ src/main.py
@@ -0,0 +1,33 @@
+from calculator import add, subtract, multiply, divide
+
+
+choix = 100
+while True:
+    print("#" * 10, "Calculatrice", "#" * 10)
+    print()
+    print("""1 - Addition " \
+    "\n2 - Soustraction \n" \
+    "3 - Division \n" \
+    "4 - Multiplication""")
+    choix = int(input("Saisissez un nombre (0 pour quitter) : "))
+    if choix == 0:
+        print("Meric d'avoir utilisé notre calculatrice. Au revoir")
+        print("#" * 40)
+        break
+    print()
+    a = float(input("premier nombre : "))
+    b = float(input("second nombre : "))
+
+    if choix == 1:
+        print("résultat :", add(a, b))
+
+    elif choix == 2:
+        print("résultat :", subtract(a, b))
+
+    elif choix == 3:
+        print("résultat :", multiply(a, b))
+
+    elif choix == 4:
+        print("résultat :", divide(a, b))
+
+    print("-" * 30)
